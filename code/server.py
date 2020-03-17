5fda750a6bc2db8d218d0d46a7a16b2adfd416fb
Eric Gertonson <eric.gert@gmail.com>
2020-03-17 16:38:27 -0500

    added initial server and html assets

---
+++ code/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("base.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
